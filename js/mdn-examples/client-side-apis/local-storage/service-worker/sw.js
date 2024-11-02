0e85e89111c14aa822f32f04208b5284ca2d497c
Ivan Pais <ivan.e.pais@gmail.com>
2024-11-01 22:42:01 -0300

    +client side storage

---
+++ js/mdn-examples/client-side-apis/local-storage/service-worker/sw.js
@@ -0,0 +1,17 @@
+cuando un navegador accede a él.
+		
+		Cuando se registra, puede controlar las páginas disponibles en ese origen.
+		
+		Para ello, se sitúa entre una página cargada y la red e intercepta las solicitudes de red dirigidas a ese origen.
+		
+		
+		Cuando intercepta una solicitud, puede hacer lo que desee
+		
+		pero el ejemplo clásico es guardar las respuestas de la red fuera de línea y luego proporcionarlas en respuesta a una solicitud en lugar de las respuestas de la red. 
+		
+		De hecho, le permite hacer que un sitio web funcione completamente sin conexión.
+
+		
+		La API de caché es otro mecanismo de almacenamiento del lado del cliente, con una pequeña diferencia
+		
+		está diseñada para guardar respuestas HTTP y, por lo tanto, funciona muy bien con los trabajadores del servicio.
